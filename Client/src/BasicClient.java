f7a5fc4207af3a7abbaa5e5e9aabde054341a76f
josh-grant <joshuagrant1996@icloud.com>
2016-01-31 00:33:41 +0000

    Added client-side session termination

--- Client/src/BasicClient.java
+++ Client/src/BasicClient.java
@@ -10,6 +10,8 @@ public class BasicClient {
 	private static Socket targetSocket;
 	private static PrintWriter targetWriter;
 	private static BufferedReader targetReader;
+	private static Console inputScanner;
+	private static String inputLine;
 
 	private static void clientInit(String initHostName, int initPortNumber) {
 		// Init
@@ -21,9 +23,11 @@ public class BasicClient {
 			System.out.println("Initialising socket connection");
 			targetSocket = new Socket(hostName, portNumber);
 			System.out.println("Connected to server");
+			System.out.println("");
 			targetWriter = new PrintWriter(targetSocket.getOutputStream(), true);
 			targetReader = new BufferedReader(
 			new InputStreamReader(targetSocket.getInputStream()));
+			inputScanner = System.console();
 		}
 		catch (IOException ex) {
 			ex.printStackTrace();
@@ -31,19 +35,32 @@ public class BasicClient {
 	}
 	private static void clientDisconnect() {
 		try {
+			targetWriter.println("Session terminated");
+			targetWriter.println("");
 			targetSocket.close();
 		}
 		catch (IOException ex) {
 			ex.printStackTrace();
 		}
 	}
+	private static void inputScanner() {
+		while (true) {
+			inputLine = inputScanner.readLine(">>");
+			if (inputLine.equals("exit()") || inputLine.equals("quit()")) {
+				System.out.println("Termination session");
+				clientDisconnect();
+				System.out.println("Session terminated");
+				break;
+			}
+			if (inputLine != null) {
+				targetWriter.println(inputLine);
+			}
+		}
+	}
 	public static void main(String[] args) {
 		// Init
 		clientInit(args[0], Integer.parseInt(args[1]));
 		// Main Loop
-		targetWriter.println(1);
-		targetWriter.println(2);
-		clientDisconnect();
-		targetWriter.println(3);
+		inputScanner();
 	}
 }
